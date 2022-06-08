3106ff801049670ed0782d099c266b01ace8f079
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-06-07 22:01:52 -0300

    on

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-        dialect: 'postgres',
-        host: 'localhost',
-        username: 'postgres',
-        password: 'docker',
-        database: 'sistema',
-        define:{
-            timestamps: true,
-            undercored: true,
-            undercoredAll: true
-        }
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'sistema',
+    define:{
+        timestamps: true,
+        undercored: true,
+        undercoredAll: true
+    }
 }
